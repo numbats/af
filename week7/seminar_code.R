db9b729609b4fec15ee879720e04d99086111926
Rob Hyndman <robjhyndman@gmail.com>
2024-04-16 12:28:02 +1000

    Week 7 seminar materials

--- week7/seminar_code.R
+++ week7/seminar_code.R
@@ -1,83 +1,66 @@
 library(fpp3)
 
-#### Amazon STOCK PRICE 2018 ----------------
+# Transformations to stationarity
 
-amazon_2018 <- gafa_stock |>
-  filter(Symbol == "AMZN", year(Date) == 2018)
+# Facebook stock
+fb <- gafa_stock |>
+  filter(Symbol == "FB") |>
+  select(Date, Close) |>
+  fill_gaps()
+fb |>
+  gg_tsdisplay(Close)
+fb |>
+  gg_tsdisplay(Close |> difference())
 
-amazon_2018 |> autoplot(Close)
-amazon_2018 |>
-  ACF(Close) |>
-  autoplot()
+# aus_production Bricks
 
-amazon_2018 |> autoplot(difference(Close)) +
-  ylab("Google closing stock price") + xlab("Day")
+aus_production |>
+  gg_tsdisplay(
+    box_cox(Bricks, lambda = 0.3)
+  )
+aus_production |>
+  gg_tsdisplay(
+    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4)
+  )
+aus_production |>
+  gg_tsdisplay(
+    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4) |> difference()
+  )
 
-amazon_2018 |>
-  ACF(difference(Close)) |>
-  autoplot()
+# Unit root tests
+fb  |> features(Close, unitroot_kpss)
+fb  |> features(Close |> difference(), unitroot_kpss)
+fb  |> features(Close, unitroot_ndiffs)
 
-amazon_2018 |>
-  features(Close, unitroot_kpss)
-amazon_2018 |>
-  features(difference(Close), unitroot_kpss)
-amazon_2018 |>
-  features(Close, unitroot_ndiffs)
+## Seasonal strength
+aus_production  |>
+  features(
+    box_cox(Bricks, lambda = 0.3),
+    feat_stl
+  )
+aus_production  |>
+  features(
+    box_cox(Bricks, lambda = 0.3),
+    unitroot_nsdiffs
+  )
 
-## A10 drugs
+## Unit root
+aus_production  |>
+  features(
+    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4),
+    unitroot_kpss
+  )
+aus_production  |>
+  features(
+    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4),
+    unitroot_ndiffs
+  )
 
-a10 <- PBS |>
-  filter(ATC2 == "A10") |>
-  summarise(Cost = sum(Cost) / 1e6)
+# Random walks
 
-a10 |> autoplot(Cost)
-
-a10 |> autoplot(log(Cost))
-
-a10 |> autoplot(
-  log(Cost) |> difference(lag = 12)
-)
-
-a10 |>
-  features(log(Cost), feat_stl)
-
-a10 |>
-  features(log(Cost) |> difference(lag=12), feat_stl)
-
-a10 |>
-  features(log(Cost), unitroot_nsdiffs)
-
-a10 |>
-  features(log(Cost) |> difference(lag=12), unitroot_ndiffs)
-
-
-## H02 drugs
-
-h02 <- PBS |>
-  filter(ATC2 == "H02") |>
-  summarise(Cost = sum(Cost) / 1e6)
-
-h02 |> autoplot(Cost)
-
-h02 |> autoplot(log(Cost))
-
-h02 |> autoplot(
-  log(Cost) |> difference(12)
-)
-
-h02 |> autoplot(
-  log(Cost) |> difference(12) |> difference(1)
-)
-
-h02 |>
-  mutate(log_sales = log(Cost)) |>
-  features(log_sales, feat_stl)
-
-h02 |>
-  mutate(log_sales = log(Cost)) |>
-  features(log_sales, unitroot_nsdiffs)
-h02 |>
-  mutate(d_log_sales = difference(log(Cost), 12)) |>
-  features(d_log_sales, unitroot_ndiffs)
-
-
+tsibble(
+  time = 1:500,
+  value = cumsum(rnorm(500) + 0.1),
+  index = time
+) |>
+  gg_tsdisplay(value)
