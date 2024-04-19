b703353fb044938dbf4763b07401b25ae2ab6ffc
Rob Hyndman <robjhyndman@gmail.com>
2024-04-19 10:32:01 +1000

    week 7 seminar code and slides

--- week7/seminar_code.R
+++ week7/seminar_code.R
@@ -4,32 +4,35 @@ library(fpp3)
 
 # Facebook stock
 fb <- gafa_stock |>
-  filter(Symbol == "FB") |>
-  select(Date, Close) |>
-  fill_gaps()
+  filter(Symbol == "FB")
 fb |>
   gg_tsdisplay(Close)
 fb |>
-  gg_tsdisplay(Close |> difference())
+  gg_tsdisplay(difference(Close))
 
 # aus_production Bricks
 
+aus_production |>
+  gg_tsdisplay(Bricks)
 aus_production |>
   gg_tsdisplay(
     box_cox(Bricks, lambda = 0.3)
   )
 aus_production |>
   gg_tsdisplay(
-    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4)
+    box_cox(Bricks, lambda = 0.3) |>
+      difference(lag = 4)
   )
 aus_production |>
   gg_tsdisplay(
-    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4) |> difference()
+    box_cox(Bricks, lambda = 0.3) |>
+      difference(lag = 4) |>
+      difference()
   )
 
 # Unit root tests
 fb  |> features(Close, unitroot_kpss)
-fb  |> features(Close |> difference(), unitroot_kpss)
+fb  |> features(difference(Close), unitroot_kpss)
 fb  |> features(Close, unitroot_ndiffs)
 
 ## Seasonal strength
@@ -43,24 +46,10 @@ aus_production  |>
     box_cox(Bricks, lambda = 0.3),
     unitroot_nsdiffs
   )
-
-## Unit root
 aus_production  |>
   features(
-    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4),
-    unitroot_kpss
-  )
-aus_production  |>
-  features(
-    box_cox(Bricks, lambda = 0.3) |> difference(lag = 4),
+    box_cox(Bricks, lambda = 0.3) |>
+      difference(lag = 4),
     unitroot_ndiffs
   )
 
-# Random walks
-
-tsibble(
-  time = 1:500,
-  value = cumsum(rnorm(500) + 0.1),
-  index = time
-) |>
-  gg_tsdisplay(value)
