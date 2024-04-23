a50c5d970c0f4c0a4b8597201b7c8861b64b9172
Rob Hyndman <robjhyndman@gmail.com>
2024-04-23 12:20:06 +1000

    Week 8 seminar

--- week8/seminar_code.R
+++ week8/seminar_code.R
@@ -9,8 +9,7 @@ global_economy |>
 
 fit <- global_economy |>
   filter(Code == "EGY") |>
-  model(ARIMA(Exports, stepwise = FALSE, approximation = FALSE,
-              order_constraint = p + q <= 10))
+  model(ARIMA(Exports))
 report(fit)
 
 gg_tsresiduals(fit)
@@ -36,4 +35,56 @@ global_economy |>
 fit1 <- global_economy |>
   filter(Code == "EGY") |>
   model(ARIMA(Exports ~ pdq(4, 0, 0)))
+
 report(fit1)
+
+
+## pelt::Lynx
+
+pelt |>
+  autoplot(Lynx)
+
+# Use a sqrt transformation to keep forecasts positive
+
+pelt |>
+  autoplot(sqrt(Lynx))
+pelt |>
+  gg_tsdisplay(sqrt(Lynx), plot_type = "partial")
+
+fit <- pelt |>
+  model(
+    auto = ARIMA(sqrt(Lynx)),
+    ar2 = ARIMA(sqrt(Lynx) ~ pdq(2,0,0)),
+    ar3 = ARIMA(sqrt(Lynx) ~ pdq(3,0,0)),
+    tryhard = ARIMA(sqrt(Lynx), stepwise = FALSE, approximation = FALSE,
+                    order_constraint = p + q <= 10)
+  )
+fit
+glance(fit) |>
+  arrange(AICc)
+fit |>
+  select(auto) |>
+  gg_tsresiduals()
+
+fit |>
+  forecast(h = 20) |>
+  autoplot(pelt, level = NULL) +
+  labs(y = "Number of Lynx", title = "Lynx Forecast")
+
+fit |>
+  select(auto) |>
+  forecast(h = 20) |>
+  autoplot(pelt) +
+  labs(y = "Number of Lynx", title = "Lynx Forecast")
+
+
+# Extract phi coefficients from best model
+phi <- fit |>
+  select(tryhard) |>
+  tidy() |>
+  pull(estimate) |>
+  head(2)
+# Is it cyclic?
+phi[1]^2 + 4*phi[2]
+# Average length of cycle
+2*pi / (acos(-phi[1] * (1 - phi[2])/(4*phi[2])))
