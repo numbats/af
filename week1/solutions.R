e898e147ff19c744159e1c22c94e86ccbd42ee6f
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-04 11:31:24 +1100

    Updated exercises for week 1

--- week1/solutions.R
+++ week1/solutions.R
@@ -1,23 +1,40 @@
+#2. Download data and create tsibble
 library(fpp3)
 
-#1. Download data
-file <- tempfile(fileext = ".xlsx")
-download.file("https://robjhyndman.com/data/tourism.xlsx", file)
-mytourism <- readxl::read_excel(file)
+aus_accommodation <- read.csv(
+  "https://workshop.nectric.com.au/user2024/data/aus_accommodation.csv"
+) |>
+  mutate(Date = as.Date(Date)) |>
+  as_tsibble(
+    index = Date,
+    key = State
+  )
+aus_accommodation
 
-#2. Create a tsibble
-mytourism <- mytourism |>
-  mutate(Quarter = yearquarter(Quarter)) |>
-  as_tsibble(index = Quarter, key = c(Region, State, Purpose))
+#3. Adjust frequency
+aus_accommodation <- read.csv(
+  "https://workshop.nectric.com.au/user2024/data/aus_accommodation.csv"
+) |>
+  mutate(Quarter = yearquarter(as.Date(Date))) |>
+  as_tsibble(
+    index = Quarter,
+    key = State
+  )
+aus_accommodation
 
-#3. Find combination of Region and Purpose with maximum number of overnight trips on average
-mytourism |>
+#4a Total quarterly visitors to Victoria
+tourism |>
+  filter(State == "Victoria") |>
+  summarise(Trips = sum(Trips))
+
+#5. Find combination of Region and Purpose with maximum number of overnight trips on average
+tourism |>
   as_tibble() |>
   group_by(Region, Purpose) |>
-  summarise(Trips = mean(Trips), .groups="drop") |>
+  summarise(Trips = mean(Trips), .groups = "drop") |>
   filter(Trips == max(Trips))
 
-#4. Create new tsibble with total number of trips by state
-state_tourism <- my_tourism |>
+#6. Create new tsibble with total number of trips by state
+tourism |>
   group_by(State) |>
   summarise(Trips = sum(Trips))
