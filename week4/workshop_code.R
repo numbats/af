33a0ae1ecd72cb6dc19b7a7d5a5989362a5bfe31
Rob J Hyndman <robjhyndman@gmail.com>
2025-03-26 09:48:19 +1100

    Week 4 workshop code

--- week4/workshop_code.R
+++ week4/workshop_code.R
@@ -1,5 +1,7 @@
 library(fpp3)
 
+## HOLIDAYS
+
 holidays <- tourism |>
   as_tibble() |>
   filter(Purpose == "Holiday") |>
@@ -9,9 +11,8 @@ holidays <- tourism |>
 # Forecasting with a transformation
 
 fc <- holidays |>
-  model(TSLM(sqrt(Trips) ~ trend() + season())
-  ) |>
-  forecast(h="5 years")
+  model(TSLM(sqrt(Trips) ~ trend() + season())) |>
+  forecast(h = "5 years")
 
 fc |>
   filter(State == "Western Australia") |>
@@ -21,26 +22,115 @@ fc |>
 
 my_dcmp_spec <- decomposition_model(
   STL(Trips),
-  NAIVE(season_adjust), # Model for seasonally adjusted series
-  SNAIVE(season_year)   # Model for seasonal component
+  MEAN(season_adjust), # Model for seasonally adjusted series
+  SNAIVE(season_year) # Model for seasonal component
 )
 
 fit <- holidays |>
   model(stl_naive = my_dcmp_spec)
 
+fc <- fit |>
+  forecast(h = "5 years")
 
-# Forecasting with a transformation and a decomposition
+fc |> filter(State == "Queensland") |> autoplot(holidays)
 
-my_dcmp_spec <- decomposition_model(
-  STL(sqrt(Trips)),
-  NAIVE(season_adjust), # Model for seasonally adjusted series
-  SNAIVE(season_year)   # Model for seasonal component
+## BRICK production
+
+bricks <- aus_production |>
+  select(Bricks) |>
+  filter(!is.na(Bricks))
+bricks |> autoplot(Bricks)
+fit <- bricks |>
+  model(stl = STL(sqrt(Bricks) ~ season(window = 13)))
+fit |>
+  components() |>
+  autoplot()
+fit |>
+  components() |>
+  as_tsibble() |>
+  autoplot(season_adjust)
+
+mdl <- decomposition_model(
+  STL(sqrt(Bricks) ~ season(window = 13), robust = TRUE),
+  NAIVE(season_adjust)
 )
+fit <- bricks |> model(mdl)
+fc <- fit |>
+  forecast(h = 24)
+fc |> autoplot(bricks)
 
-fc <- holidays |>
-  model(stl_naive = my_dcmp_spec) |>
-  forecast(h="5 years")
+## AFGHANISTAN population
+
+global_economy |>
+  filter(Country == "Afghanistan") |>
+  autoplot(Population)
+
+fit <- global_economy |>
+  filter(Country == "Afghanistan") |>
+  model(
+    linear = TSLM(Population ~ trend()),
+    piecewise = TSLM(Population ~ trend(knots = c(1980, 1989)))
+  )
+fit |>
+  select(piecewise) |>
+  report()
+
+fc <- fit |>
+  forecast(h = 5)
 
 fc |>
-  filter(State == "Victoria") |>
-  autoplot(holidays)
+  autoplot(global_economy)
+
+
+## OLYMPIC RUNNING
+
+olympic_running |>
+  as_tibble() |>
+  ggplot(aes(x = Year, y = Time, colour = Sex)) +
+  geom_line() +
+  facet_wrap(~Length, scales = "free_y")
+
+fit <- olympic_running |>
+  model(
+    linear = TSLM(Time ~ trend()),
+  )
+
+tidy(fit) |>
+  filter(term == "trend()") |>
+  mutate(change_per_year = estimate / 4) |>
+  select(Length, Sex, change_per_year)
+
+fit |>
+  forecast(h = 3) |>
+  as_tibble() |>
+  filter(Year == 2028) |>
+  select(Length, Sex, .mean)
+
+# These are all world records, and are very unlikely
+
+# Women have not been competing for as long as men and need
+# different models
+male_fc <- olympic_running |>
+  filter(Sex == "men") |>
+  model(
+    pwlinear = TSLM(Time ~ trend(knots = c(1920, 1990))),
+  ) |>
+  forecast(h = 3)
+female_fc <- olympic_running |>
+  filter(Sex == "women") |>
+  model(
+    pwlinear = TSLM(Time ~ trend(knots = c(1980))),
+  ) |>
+  forecast(h = 3)
+fc <- bind_rows(male_fc, female_fc)
+fc |>
+  as_tibble() |>
+  ggplot(aes(x = Year, y = .mean, colour = Sex)) +
+  geom_line() +
+  geom_line(data = olympic_running, aes(y = Time), linetype = "dashed") +
+  facet_wrap(~Length, scales = "free_y")
+
+# Warnings from cases where no data available before a knot
+# In most cases, these forecasts look much better, although the upward trend
+# in some cases is unrealistic. A better model would be either downward trending
+# or flat in the forecast period.
