2150825bb3712f7f04527c38da108042ce70ac48
Rob J Hyndman <robjhyndman@gmail.com>
2025-04-08 15:34:57 +1000

    Updated workshop 6 code

--- week6/workshop_code.R
+++ week6/workshop_code.R
@@ -18,9 +18,15 @@ fit <- chinese_gdp |>
     #beta0 =  ETS(GDP_pc ~ error("A") + trend("A", beta = 0)),
     #beta_gt_alpha =  ETS(GDP_pc ~ error("A") +
     #                       trend("A", alpha = 0.3, beta = 0.31)),
-    AAN = ETS(GDP_pc ~ error("A") + trend("A")),
-    AAdN = ETS(GDP_pc ~ error("A") + trend("Ad")),
-    ANN = ETS(GDP_pc ~ error("A") + trend("N")),
+    #AAN = ETS(GDP_pc ~ error("A") + trend("A")),
+    #AAdN = ETS(GDP_pc ~ error("A") + trend("Ad")),
+    #ANN = ETS(GDP_pc ~ error("A") + trend("N")),
+    #MAN = ETS(GDP_pc ~ error("M") + trend("A")),
+    #MAdN = ETS(GDP_pc ~ error("M") + trend("Ad")),
+    #MNN = ETS(GDP_pc ~ error("M") + trend("N")),
+    #BC = ETS(box_cox(GDP_pc, lambda = 0.3) ~ error("A") + trend("A")),
+    ets1 = ETS(GDP_pc ~ error("A")),
+    ets2 = ETS(GDP_pc)
   )
 
 fit
@@ -32,9 +38,12 @@ fit |>
 fc <- fit |> forecast(h=10)
 
 fc |>
-  filter(.model == "AAN") |>
-  autoplot(chinese_gdp)
+  #filter(.model == "AAN") |>
+  autoplot(chinese_gdp, level = NULL)
 
+fc |>
+  filter(.model %in% c("BC", "MAN")) |>
+  autoplot(chinese_gdp)
 
 
 
