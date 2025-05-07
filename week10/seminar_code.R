5cadf6d8125b2e484cdab4de271047c3669526b2
Rob J Hyndman <robjhyndman@gmail.com>
2025-05-07 15:06:31 +1000

    Week 10 seminar code

--- week10/seminar_code.R
+++ week10/seminar_code.R
@@ -17,44 +17,46 @@ leisure |>
   )
 
 leisure |>
-  gg_tsdisplay(difference(Employed, 12),
-               plot_type = "partial", lag = 36
-  ) +
+  gg_tsdisplay(difference(Employed, 12), plot_type = "partial", lag = 36) +
   labs(title = "Seasonally differenced", y = "")
 
 leisure |>
-  gg_tsdisplay(difference(Employed, 12) |> difference(),
-               plot_type = "partial", lag = 36
+  gg_tsdisplay(
+    difference(Employed, 12) |> difference(),
+    plot_type = "partial",
+    lag = 36
   ) +
   labs(title = "Double differenced", y = "")
 
-fit <- leisure |>
+leisure_fit <- leisure |>
   model(
     arima012011 = ARIMA(Employed ~ pdq(0, 1, 2) + PDQ(0, 1, 1)),
     arima210011 = ARIMA(Employed ~ pdq(2, 1, 0) + PDQ(0, 1, 1)),
     auto = ARIMA(Employed),
     best = ARIMA(Employed, stepwise = FALSE, approx = FALSE)
   )
-fit
-fit |> pivot_longer(everything(),
-                    names_to = "Model name",
-                    values_to = "Orders"
-)
+leisure_fit
+leisure_fit |>
+  pivot_longer(everything(), names_to = "Model name", values_to = "Orders")
 
-glance(fit) |>
+glance(leisure_fit) |>
   arrange(AICc) |>
   select(.model:BIC)
 
-fit |>
+leisure_fit |>
+  select(best) |>
+  report()
+
+leisure_fit |>
   select(best) |>
   gg_tsresiduals(lag = 36)
 
-report(fit |> select(best))
-augment(fit) |>
+leisure_fit |> select(best) |> report()
+augment(leisure_fit) |>
   filter(.model == "best") |>
   features(.innov, ljung_box, lag = 24, dof = 4)
 
-forecast(fit, h = 36) |>
+forecast(leisure_fit, h = 36) |>
   filter(.model == "best") |>
   autoplot(leisure) +
   labs(
@@ -62,45 +64,42 @@ forecast(fit, h = 36) |>
     y = "Number of people (millions)"
   )
 
+
 # QUARTERLY CEMENT ETS vs ARIMA
 
 cement <- aus_production |>
   select(Cement) |>
   filter_index("1988 Q1" ~ .)
 cement |> autoplot(Cement)
+
+# Fit ETS and ARIMA models to a training set and compare forecasts on test set
 train <- cement |> filter_index(. ~ "2007 Q4")
 fit <- train |>
   model(
     arima = ARIMA(Cement),
     ets = ETS(Cement)
   )
-
+# Residual diagnostic for ETS model
 fit |>
   select(ets) |>
   report()
 gg_tsresiduals(fit |> select(ets), lag_max = 16)
-
 fit |>
   select(ets) |>
   augment() |>
   features(.innov, ljung_box, lag = 16)
-
+# Residual diagnostic for ARIMA model
 fit |>
   select(arima) |>
   report()
 gg_tsresiduals(fit |> select(arima), lag_max = 16)
-
 fit |>
   select(arima) |>
   augment() |>
   features(.innov, ljung_box, lag = 16, dof = 6)
 
-fit |>
-  forecast(h = "2 years 6 months") |>
-  accuracy(cement)
-
-fc <- cement |>
-  model(arima = ARIMA(Cement)) |>
+# Forecasts
+fc <- fit |>
   forecast(h = "3 years")
 fc |>
   autoplot(cement) +
@@ -108,3 +107,25 @@ fc |>
     title = "Cement production in Australia",
     y = "Tonnes ('000)"
   )
+fc |> accuracy(cement)
+
+# Compare models using tscv
+
+cement_stretch <- cement |>
+  stretch_tsibble(.init = 24, .step = 1)
+fit_stretch <- cement_stretch |>
+  model(
+    ets = ETS(Cement),
+    arima = ARIMA(Cement)
+  )
+fc_stretch <- fit_stretch |>
+  forecast(h = "2 years") |>
+  group_by(.id, .model) |>
+  mutate(h = row_number()) |>
+  ungroup() |>
+  as_fable(response = "Cement", distribution = Cement)
+
+fc_stretch |>
+  accuracy(cement, by = c("h", ".model")) |>
+  ggplot(aes(x = h, y = RMSE, color = .model)) +
+  geom_line()
